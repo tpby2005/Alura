4b58afeee1fca872d05f10ec3736847655abac24
tpby2005 <toby.bartholovitsch@gmail.com>
2023-06-14 17:27:23 -0700

    Added newline

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+$HOME/opt/cross/bin/i686-elf-as boot.s -o boot.o
+$HOME/opt/cross/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+$HOME/opt/cross/bin/i686-elf-gcc -T linker.ld -o alura.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+
+cp alura.bin iso/boot/alura.bin
+cp grub.cfg iso/boot/grub/grub.cfg
+grub-mkrescue -o alura.iso iso
+qemu-system-i386 -cdrom alura.iso
