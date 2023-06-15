92c2bb51fa9f0a9ba4885d3d39a169064aea1ab9
tpby2005 <toby.bartholovitsch@gmail.com>
2023-06-14 19:01:34 -0700

    updated scripts

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
+./clean.sh
+
 $HOME/opt/cross/bin/i686-elf-as boot.s -o boot.o
-$HOME/opt/cross/bin/i686-elf-gcc -c kernel/kernel.c -o kernel/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-$HOME/opt/cross/bin/i686-elf-gcc -T linker.ld -o alura.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+$HOME/opt/cross/bin/i686-elf-gcc -c lib/string/strlen.c -o lib/string/strlen.o -std=gnu99 -ffreestanding -O2
+$HOME/opt/cross/bin/i686-elf-gcc -c kernel/tty.c -o kernel/tty.o -std=gnu99 -ffreestanding -O2
+$HOME/opt/cross/bin/i686-elf-gcc -c kernel/kernel.c -o kernel/kernel.o -std=gnu99 -ffreestanding -O2
+$HOME/opt/cross/bin/i686-elf-gcc -T linker.ld -o alura.bin -ffreestanding -O2 -nostdlib boot.o kernel/kernel.o kernel/tty.o lib/string/strlen.o -lgcc
 
 mkdir -p iso/boot/grub
 
---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+rm -rf iso/boot/alura.bin
+rm -rf kernel/kernel.o
+rm -rf kernel/tty.o
+rm -rf lib/string/strlen.o
+rm -rf alura.bin
+rm -rf alura.iso
+rm -rf boot.o
