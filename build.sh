09e3384c936c1c51e4126b7ddec66256e100dc85
tpby2005 <toby.bartholovitsch@gmail.com>
2023-06-14 17:47:47 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ $HOME/opt/cross/bin/i686-elf-as boot.s -o boot.o
 $HOME/opt/cross/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 $HOME/opt/cross/bin/i686-elf-gcc -T linker.ld -o alura.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 
+mkdir -p iso/boot/grub
+
 cp alura.bin iso/boot/alura.bin
 cp grub.cfg iso/boot/grub/grub.cfg
 grub-mkrescue -o alura.iso iso
